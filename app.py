2d04aee2446939a965685f2ee16a2d895fcbfe8d
Rushikesh Mhaske <rushikeshmhaske61@gmail.com>
2026-09-20 16:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -902,8 +902,9 @@ elif page == "🔍 Predict Sign":
     input_method = st.radio(
         "How do you want to provide the image?",
         [
+            
+            "📁 Upload File",
             "📷 Open Camera",
-            "📁 Upload File"
         ],
         horizontal=True
     )
